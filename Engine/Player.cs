5a3733036c6afb24654d3fdf8897b4fa095ebce9
fccJashCoda <personalrevolution2014@gmail.com>
2020-11-30 23:15:49 +0100

    most classes are set

---
+++ Engine/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+  public class Player : LivingCreature
+  {
+    public int Gold { get; set; }
+    public int Experience { get; set; }
+    public int Level { get; set; }
+    public Player(int currentHitPoints, int maximumHitPoints, int gold, int experience, int level) : base(currentHitPoints, maximumHitPoints)
+    {
+      Gold = gold;
+      Experience = experience;
+      Level = level;
+    }
+  }
+}
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Engine;
 
 namespace SuperAdventure
 {
   public partial class SuperAdventure : Form
   {
+
+    private Player _player;
     public SuperAdventure()
     {
       InitializeComponent();
+
+      _player = new Player(10, 10, 20, 0, 1);
+
+      lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+      lblGold.Text = _player.Gold.ToString();
+      lblExperience.Text = _player.Experience.ToString();
+      lblLevel.Text = _player.Level.ToString();
+
+      Location location = new Location(1, "Home", "This is your house");
+    }
+
+    private void SuperAdventure_Load(object sender, EventArgs e)
+    {
+
     }
   }
 }
